488e894a17f87018a0ed2558d322a12074229609
Marc Phillips <derigible@gmail.com>
2018-12-13 11:07:59 -0700

    Family bio added 2018

--- src/family/data/familyInfo.js
+++ src/family/data/familyInfo.js
@@ -2,77 +2,62 @@ export default {
   '2018': [
     {
       type: 'text',
-      data: `With the close of another year comes another summary of what has
-      happened in the lives of our little family. Several things happened these
-      past months which were both challenging, fun, tedious at times, and altogether
-      character expanding. From multiple house projects, to the wonderful news
-      that we will be joined by another child in July of 2018, 2017 has been
-      a very eventful time for us.`
+      data: `Another year, another baby. At least, that is the crowning highlight of this
+      year. Brinley joined us in late June and has made a great impact on all of us. From
+      more nights of poor sleep, to lots of smiles and laughs, our family's expansion has
+      been a growing experience (pun intended).`
     },
     {
       type: 'text',
-      data: `In January we moved into our new house in Midvale where we immediately
-      set to work trying to clean the place after what seemed like several years of
-      neglect. Our fixer upper ambitions were quickly curtailed by the punishment of
-      living in the house you are trying to renovate. We heard it before, and so have
-      many others, but it bears repeating: do not live in the house you plan to
-      fix up until after the work is done. It has been trying and mentally and
-      emotionally taxing to say the least. However, the end is approaching and all
-      the work is starting to really come together, making for a beautiful home
-      several months after starting our projects.`
+      data: `Other than new children, not nearly as much has happened as previous years as
+      we decided that this year we would try and just live "normal" lives. For example, we
+      didn't move, buy a home, or try and find a new job. We just learned how to live with
+      the blessings we have, and how to balance the crazy growth we have experienced over
+      the last five years. We have, after all, grown at an average rate of 12% per year (3
+      children in 5 years). From that statement it is clear we also celebrated our 5 years
+      of marriage by... going to see a children's movie without the kids and spending the
+      night without them. We already act like an old couple...`
     },
     {
       type: 'text',
-      data: `All the changes have brought a newfound appreciation for consistency.
-      Thankfully we have been able to enjoy the proximity of family and good friends
-      in the ward that have helped us with Marley and Whitley when we were busy working.
-      Perhaps the biggest blessing of this move is the friendships we have made with
-      our neighbors, something we really didn't do very well in our last area. Whitley
-      especially has been blessed with having a cute little girl her age named Gigi
-      to play with who lives next door. Marley even has a friend a few doors down her
-      age who both her and Whitley adore being with. With families who have children
-      our kids ages, and with adults in the same stages of life as we are, this move has
-      been a good one for us and given us consistent support when we needed it.`
+      data: `Of course we didn't just sit around at home and do nothing. Andrea and the girls
+      were given Aquarium passes this year and have spent several days walking around and
+      seeing all the fishes, cats, and other animals, and playing in the play center there.
+      The library has also been a regular weekly visit where the girls pick out about 30 books
+      and come home and pretty much have read all of them by the time Dad comes home from work.
+      Wheeler farm also continues to be a near weekly destination, as well as trips to Costco.
+      Perhaps the biggest blessing to our family this year is having Grandma Carter watch the girls
+      a few hours each week so Andrea and I can go out and do whatever. It has been great seeing
+      their relationship grow with their grandparents (and uncle Jared, who lives with them for
+      the time being).`
     },
     {
       type: 'text',
-      data: `Of course, the girls have helped make our lives so much richer that the
-      pleasantness of the area is only just a bonus. Watching Whitley and Marley play
-      together is a true joy, especially as Marley has grown in understanding and
-      opinions (and quite the opinions she has). Often playtime consists of screams,
-      song, dancing, more screams, and fits of giggles and laughter. The two really
-      love each other and it is great seeing their friendship grow. Whitley loves to have
-      a sister who she can play house with, and Marley loves to do whatever Whitley
-      is doing. It will be interesting to see how they respond together with our new
-      child. We hope that the fights over who sits on mommy's lap will cease when it
-      becomes clear to them that neither will win out over the baby.`
+      data: `Some highlights of the year are going to Yellowstone with the kids and Jamison and
+      Loralie and Benjamin. Brinley was with us at that point, so she got to see all the cool
+      things there from the comfort of her carseat. We also purchased a brand new van in prep
+      for Brinley, and that has made long trips much more pleasant for everyone. We also went
+      to Crystal Hot Springs for the first time and the girls loved it. We also went to the
+      Festival of Trees for the first time to see Whitley dance, which was wonderful and adorable
+      and induced much pride in her.`
     },
     {
       type: 'text',
-      data: `Since so much of our free time is taken by the girls, it is fitting that
-      we have learned to really enjoy walks and playing at the park. The girls really
-      enjoy the seesaw, the slides, and the climbing rocks at the local playground. But
-      often the thing they enjoy most is watching the other kids play. Whitley has started
-      to really break out of her shell this year and isn't nearly as terrified of the
-      other kids, and Marley has of course followed suit. These playground outings,
-      as well as trips to the library or the splash pad in summer, constitute most
-      of the outdoor adventure the girls get to enjoy. We did have one camping trip
-      with some friends of ours in late summer that we enjoyed and the girls loved as
-      well. Next we will try hiking and see if they like that.`
+      data: `House work dominated the beginning half of the year, with changes to the floor
+      being done throughout the entirety of the top floor. Lots of other little projects have
+      also occurred as well. It has been enough that we are finally done with trying to do it
+      by ourselves only and have learned to not spread ourselves too thin. Growing has also
+      taught us to cut back. We have also learned to count our blessings more often and be
+      grateful for what we do have and not focus on what we could have.`
     },
     {
       type: 'text',
-      data: `Our little family is starting to not be so little anymore, and with it
-      comes challenges and also blessings. God has been gracious to us in keeping us
-      safe despite the chaos of our lives. Several times we have had narrow calls
-      only to come out relatively unscathed. Perhaps the person who took the most
-      bruises and tumbles this year was Marley, having gone a solid two months with
-      at least one scratch or bruise on her head (and all of them were because she
-      refuses to slow down when she walks - you can read more about her craziness
-      by clicking on her picture above). But that sort of mentality really has summed
-      up our life - we refuse to slow down until we emotionally collapse. Hopefully
-      next year we will learn to pace ourselves better - and avoid the mental
-      bruises like we sustained this year along the way.`
+      data: `The Lord has blessed us significantly this year. It has not been an easy year
+      by any measure of the word, but it has certainly seen a lot of growth in our lives.
+      Needless to say we are excited to see this year end and are so grateful to be taking
+      the blessings we received with us into next year, not the least of which is Brinley.
+      We can't wait to see how the relationship between our little girls continues to develop
+      and grow.`
     }
   ],
   '2017': [
